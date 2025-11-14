c88d53135e77cdfee273f04431048dbf0c3dea49
Dione F. P. <dionepinheiro6030@gmail.com>
2025-11-14 17:19:10 -0300

    ✨Adicionando menu interativo

---
+++ src/Main.ts
@@ -0,0 +1,34 @@
+import Prompt from "prompt-sync";
+
+const teclado = Prompt();
+
+while (true) {
+    console.log("+------------------Menu------------------+");
+    console.log("|1. Opções de Membros                    |");
+    console.log("|2. Opções de Livros                     |");
+    console.log("|3. Opções de Empréstimos                |");
+    console.log("|0. Sair                                 |");
+    console.log("+----------------------------------------+");
+    const escolha =+ teclado("Escolha uma opção: ");
+
+    if (escolha === 0) {
+        console.log("Saindo...");
+        break;
+    };
+
+    switch (escolha) {
+        case 1: {
+            throw new Error("Not implemented yet: Membros");
+        }
+        case 2: {
+            throw new Error("Not implemented yet: Livros");
+        }
+        case 3: {
+            throw new Error("Not implemented yet: Empréstimos");
+        }
+        default: {
+            console.log("Opção inválida!");
+            break;
+        };
+    };
+};
