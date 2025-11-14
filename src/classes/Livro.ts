7282c3aa773d8a2ac07f19ede641db0cf7d2405c
Dione F. P. <dionepinheiro6030@gmail.com>
2025-11-14 15:44:18 -0300

    ✅Adicionando Seters

--- src/classes/Livro.ts
+++ src/classes/Livro.ts
@@ -21,14 +21,36 @@ export class Livro implements LivrosDAO {
     get titulo(): string {
         return this._titulo;
     }
+    set titulo(titulo: string) {
+        if (titulo.trim() === "") throw new Error("O título não pode ser vazio.");
+        if (titulo.length > 40) throw new Error("O título não pode ter mais de 40 caracteres.");
+        this._titulo = titulo;
+    }
+
     get autor(): string {
         return this._autor;
     }
+    set autor(autor: string) {
+        if (autor.trim() === "") throw new Error("O autor não pode ser vazio.");
+        if (autor.length < 3 || autor.length > 40) throw new Error("O autor deve ter entre 3 e 40 caracteres.");
+        this._autor = autor;
+    }
+
     get ISBN(): string {
         return this._ISBN;
     }
+    set ISBN(ISBN: string) {
+        if (ISBN.trim() === "") throw new Error("O ISBN não pode ser vazio.");
+        if (ISBN.length !== 13) throw new Error("O ISBN deve ter exatamente 13 caracteres.");
+        this._ISBN = ISBN;
+    }
+
     get anoPublicacao(): Date {
         return this._anoPublicacao;
+    }
+    set anoPublicacao(anoPublicacao: Date) {
+        if (anoPublicacao > new Date()) throw new Error("O ano de publicação não pode ser no futuro.");
+        this._anoPublicacao = anoPublicacao;
     }
 
     adicionar(livro: Livro): Livro {
