a52bdbaa7412654293c80e6adb5de759854a054e
Dione F. P. <dionepinheiro6030@gmail.com>
2025-11-19 16:33:43 -0300

    ✨ Refatorando a função menuPrincipal

--- src/Main.ts
+++ src/Main.ts
@@ -24,25 +24,45 @@ async function verificarAPI(): Promise<void> {
     }
 }
 
+async function menuPrincipal(): Promise<void> {
+    await verificarAPI();
+    while (true) {
+        console.log("+------------------Menu------------------+");
+        console.log("|1. Opções de Membros                    |");
+        console.log("|2. Opções de Livros                     |");
+        console.log("|3. Opções de Empréstimos                |");
+        console.log("|0. Sair                                 |");
+        console.log("+----------------------------------------+");
+        const escolha = +teclado("Escolha uma opção: ");
 
-    if (escolha === 0) {
-        console.log("Saindo...");
-        break;
-    };
-
-    switch (escolha) {
-        case 1: {
-            throw new Error("Not implemented yet: Membros");
-        }
-        case 2: {
-            throw new Error("Not implemented yet: Livros");
-        }
-        case 3: {
-            throw new Error("Not implemented yet: Empréstimos");
-        }
-        default: {
-            console.log("Opção inválida!");
+        if (escolha === 0) {
+            console.log("Saindo...");
             break;
-        };
-    };
-};
+        }
+
+        switch (escolha) {
+            case 1: {
+                await menuMembros();
+                break;
+            }
+            case 2: {
+                console.log("Funcionalidade de Livros ainda não implementada.");
+                break;
+            }
+            case 3: {
+                console.log("Funcionalidade de Empréstimos ainda não implementada.");
+                break;
+            }
+            default: {
+                console.error("Opção inválida!");
+                break;
+            }
+        }
+    }
+}
+
+            break;
+
+menuPrincipal().catch(err => {
+    console.error("Falha fatal:", err);
+});
