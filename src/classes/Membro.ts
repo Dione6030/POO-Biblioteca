745dc074f682da7374c1ef434bcbd14d9bab293f
Dione F. P. <dionepinheiro6030@gmail.com>
2025-11-19 14:30:24 -0300

    ✨ Adicionando validações nos setters de Membro para garantir dados corretos

--- src/classes/Membro.ts
+++ src/classes/Membro.ts
@@ -17,37 +17,32 @@ export class Membro extends Pessoa {
     public static daInterface(dto: MembroDTO): Membro {
         return new Membro(dto.idPessoa, dto.nome, dto.numeroMatricula, dto.endereco, dto.telefone);
     }
+
+    get idPessoa(): number { return this._idPessoa; }
+
+    get nome(): string { return this._nome; }
     set nome(nome: string) {
-        if (nome.trim() === "" ) throw new Error("Você deve colocar o nome completo");
+        if (nome.trim() === "") throw new Error("Você deve colocar o nome completo");
         if (nome.length < 3 || nome.length > 40) throw new Error("Nome deve ter entre 3 e 40 caracteres");
-        this.nome = nome;
-    }
-    get nome(): string {
-        return this.nome;
+        this._nome = nome;
     }
     
+    get numeroMatricula(): string { return this._numeroMatricula; }
     set numeroMatricula(numero: string) {
         if (numero.trim() === "") throw new Error("Você deve colocar o número de matrícula");
-        this.numeroMatricula = numero;
-    }
-    get numeroMatricula(): string {
-        return this.numeroMatricula;
+        this._numeroMatricula = numero;
     }
 
+    get endereco(): string { return this._endereco; }
     set endereco(endereco: string) {
         if (endereco.trim() === "") throw new Error("Você deve colocar o endereço");
-        this.endereco = endereco;
-    }
-    get endereco(): string {
-        return this.endereco;
+        this._endereco = endereco;
     }
 
+    get telefone(): string { return this._telefone; }
     set telefone(telefone: string) {
         if (telefone.trim() === "") throw new Error("Você deve colocar o telefone");
-        this.telefone = telefone;
-    }
-    get telefone(): string {
-        return this.telefone;
+        this._telefone = telefone;
     }
 
     public adicionar(pessoa: Pessoa): Pessoa {
